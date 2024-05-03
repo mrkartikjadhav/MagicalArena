93b3631bc8561f7bf9af01e081f967dbfc6c4e5c
Kartik Jadhav <ikartikjadhav@gmail.com>
2024-05-03 17:57:13 +0530

    variables and constructor created

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player{
+    private int health;
+    private int strength;
+    private int attack;
+
+    public Player(int health, int strength, int attack){
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+}
