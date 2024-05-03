459d2a28b7db94dc59a45a51ea15c01b6cc0159f
Kartik Jadhav <ikartikjadhav@gmail.com>
2024-05-03 18:12:37 +0530

    Created player class variables and constructor of MagicalArena class

--- MagicalArena.java
+++ MagicalArena.java
@@ -1,3 +1,10 @@
 public class MagicalArena {
+    private Player PlayerA;
+    private Player PlayerB;
+
+    public MagicalArena(Player PlayerA, Player PlayerB) {
+        this.PlayerA = PlayerA;
+        this.PlayerB = PlayerB;
+    }
     
 }
